aa673fdb690a39a24340953ab87e10aef5370119
Alex Boylan <aboylan@users.noreply.github.com>
2023-08-15 22:41:57 -0600

    Creando la vista listar.html

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Alex', 'Boylan', 'boylan@testmail.com', '2023-08-15');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'John', 'Doe', 'john.doe@gmail.com', '2023-08-15');
