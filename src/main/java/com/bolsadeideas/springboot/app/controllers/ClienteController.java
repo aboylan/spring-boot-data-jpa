c82ef5d44e53c47b4c127674f37a3294e0995fa4
Alex Boylan <aboylan@users.noreply.github.com>
2023-08-20 22:07:42 -0600

    Agregar directorio absoluto y externo en raiz del proyecto

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -5,7 +5,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -35,6 +38,8 @@ public class ClienteController {
 
 	@Autowired
 	private IClienteService clienteService;
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	@GetMapping(value = "/ver/{id}")
 	public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
@@ -102,14 +107,18 @@ public class ClienteController {
 		}
 
 		if (!foto.isEmpty()) {
-			String rootPath = "F://Temp//uploads";
-			try {
-				byte[] bytes = foto.getBytes();
-				Path rutaCompleta = Paths.get(rootPath + "//" + foto.getOriginalFilename());
-				Files.write(rutaCompleta, bytes);
-				flash.addFlashAttribute("info", "Hsa subido correctamente '" + foto.getOriginalFilename() + "'");
+			String uniqueFilename = UUID.randomUUID().toString() + "_" + foto.getOriginalFilename();
+			Path rootPath = Paths.get("uploads").resolve(uniqueFilename);
+			Path rootAbsolutPath = rootPath.toAbsolutePath();
 
-				cliente.setFoto(foto.getOriginalFilename());
+			log.info("rootPath: " + rootPath);
+			log.info("rootAbsolutPath: " + rootAbsolutPath);
+
+			try {
+				Files.copy(foto.getInputStream(), rootAbsolutPath);
+				flash.addFlashAttribute("info", "Hsa subido correctamente '" + uniqueFilename + "'");
+
+				cliente.setFoto(uniqueFilename);
 
 			} catch (IOException e) {
 				e.printStackTrace();
