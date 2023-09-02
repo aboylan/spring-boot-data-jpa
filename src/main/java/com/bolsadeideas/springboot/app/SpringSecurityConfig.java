4b1f2a01ebb6b8223e056f9d5661c2876054f71a
Alex Boylan <aboylan@users.noreply.github.com>
2023-09-01 20:58:29 -0600

    Anadiendo metodo configure(HttpSecurity http) para las reglas ACL en las rutas

--- src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -2,10 +2,15 @@ package com.bolsadeideas.springboot.app;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -28,4 +33,23 @@ public class SpringSecurityConfig {
 		return manager;
 	}
 
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+
+		MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+
+		http.authorizeHttpRequests((authz) -> {
+			authz.requestMatchers(mvcMatcherBuilder.pattern("/"), new AntPathRequestMatcher("/css/**"),
+					new AntPathRequestMatcher("/js/**"), new AntPathRequestMatcher("/images/**"),
+					mvcMatcherBuilder.pattern("/listar")).permitAll()
+					.requestMatchers(mvcMatcherBuilder.pattern("/ver/**")).hasAnyRole("USER")
+					.requestMatchers(mvcMatcherBuilder.pattern("/uploads/**")).hasAnyRole("USER")
+					.requestMatchers(mvcMatcherBuilder.pattern("/form/**")).hasAnyRole("ADMIN")
+					.requestMatchers(mvcMatcherBuilder.pattern("/eliminar/**")).hasAnyRole("ADMIN")
+					.requestMatchers(mvcMatcherBuilder.pattern("/factura/**")).hasAnyRole("ADMIN").anyRequest()
+					.authenticated();
+		});
+
+		return http.build();
+	}
 }
