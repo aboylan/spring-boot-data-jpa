c85dd3bcbb165f76a2cdb3dd4333f9dd93ced52d
Alex Boylan <aboylan@users.noreply.github.com>
2023-09-13 23:35:30 -0600

    Mejorando el diseno y look and feel de la vista PDF

--- src/main/java/com/bolsadeideas/springboot/app/view/pdf/FacturaPdfView.java
+++ src/main/java/com/bolsadeideas/springboot/app/view/pdf/FacturaPdfView.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.app.view.pdf;
 
+import java.awt.Color;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
@@ -28,13 +29,24 @@ public class FacturaPdfView extends AbstractPdfView {
 
 		PdfPTable tabla = new PdfPTable(1);
 		tabla.setSpacingAfter(20);
-		tabla.addCell("Datos del Cliente");
+
+		PdfPCell cell = null;
+		cell = new PdfPCell(new Phrase("Datos del Cliente"));
+		cell.setBackgroundColor(new Color(184, 218, 255));
+		cell.setPadding(8f);
+		tabla.addCell(cell);
+
 		tabla.addCell(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido());
 		tabla.addCell(factura.getCliente().getEmail());
 
 		PdfPTable tabla2 = new PdfPTable(1);
-		tabla.setSpacingAfter(20);
-		tabla2.addCell("Datos de la Factura");
+		tabla2.setSpacingAfter(20);
+
+		cell = new PdfPCell(new Phrase("Datos de la Factura"));
+		cell.setBackgroundColor(new Color(195, 230, 203));
+		cell.setPadding(8f);
+
+		tabla2.addCell(cell);
 		tabla2.addCell("Folio: " + factura.getId());
 		tabla2.addCell("Descripcion: " + factura.getDescripcion());
 		tabla2.addCell("Fecha: " + factura.getCreateAt());
@@ -43,6 +55,7 @@ public class FacturaPdfView extends AbstractPdfView {
 		document.add(tabla2);
 
 		PdfPTable tabla3 = new PdfPTable(4);
+		tabla3.setWidths(new float[] { 3.5f, 1, 1, 1 });
 		tabla3.addCell("Producto");
 		tabla3.addCell("Precio");
 		tabla3.addCell("Cantidad");
@@ -51,16 +64,19 @@ public class FacturaPdfView extends AbstractPdfView {
 		for (ItemFactura item : factura.getItems()) {
 			tabla3.addCell(item.getProducto().getNombre());
 			tabla3.addCell(item.getProducto().getPrecio().toString());
-			tabla3.addCell(item.getCantidad().toString());
+			
+			cell = new PdfPCell(new Phrase(item.getCantidad().toString()));
+			cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+			tabla3.addCell(cell);
 			tabla3.addCell(item.calcularImporte().toString());
 		}
 
-		PdfPCell cell = new PdfPCell(new Phrase("Total: "));
+		cell = new PdfPCell(new Phrase("Total: "));
 		cell.setColspan(3);
 		cell.setHorizontalAlignment(PdfCell.ALIGN_RIGHT);
 		tabla3.addCell(cell);
 		tabla3.addCell(factura.getTotal().toString());
-		
+
 		document.add(tabla3);
 	}
 
